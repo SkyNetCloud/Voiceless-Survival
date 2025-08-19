be9e7ed10769b293511bde776322401dfff718f9
SkyNetCloud <mcboxer1201@gmail.com>
2025-08-18 23:23:26 -0400

    Fixed animal not running away when hearing the player

--- modules/fabric_1.21.4/src/main/java/com/armilp/ezvcsurvival/goals/injector/RunAwayVoiceGoalInjector.java
+++ modules/fabric_1.21.4/src/main/java/com/armilp/ezvcsurvival/goals/injector/RunAwayVoiceGoalInjector.java
@@ -1,13 +1,11 @@
 package com.armilp.ezvcsurvival.goals.injector;
 
-import com.armilp.ezvcsurvival.EZVCSurvival;
 import com.armilp.ezvcsurvival.config.VoiceConfig;
 import com.armilp.ezvcsurvival.goals.RunawayVoiceGoal;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.entity.ai.goal.GoalSelector;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 
 import java.lang.reflect.Field;
@@ -24,12 +22,12 @@ public class RunAwayVoiceGoalInjector {
         private static void registerEntityLoadListener() {
             ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
                 if (entity instanceof AnimalEntity animal) {
-                    addRunwayVoiceGoal(animal,world);
+                    addRunwayVoiceGoal(animal);
                 }
             });
         }
 
-        private static void addRunwayVoiceGoal(AnimalEntity animal, ServerWorld world) {
+        private static void addRunwayVoiceGoal(AnimalEntity animal) {
             Map<String, Map<String, Double>> configs = VoiceConfig.getAnimalVoiceConfigs();
             Identifier animalId = ENTITY_TYPE.getId(animal.getType());
 
@@ -38,16 +36,15 @@ public class RunAwayVoiceGoalInjector {
                 double speed = config.getOrDefault("speed", 1.0);
                 double range = config.getOrDefault("range", 16.0);
                 double threshold = config.getOrDefault("threshold", -40.0);
-                if (world.getPlayers(player -> player.interactionManager.getGameMode().isSurvivalLike()).isEmpty()) {
-                    try{
+
+                try{
                         Field goalSelectorField = MobEntity.class.getDeclaredField("goalSelector");
                         goalSelectorField.setAccessible(true);
                         GoalSelector goalSelector = (GoalSelector) goalSelectorField.get(animal);
                         goalSelector.add(1, new RunawayVoiceGoal(animal, speed, (int) range,threshold));
-                        EZVCSurvival.LOGGER.info("Goal is being Added");
-                    } catch (Exception ignored){
+                        //EZVCSurvival.LOGGER.info("Goal is being Added");
+                } catch (Exception ignored){
 
-                    }
                 }
             }
         }
