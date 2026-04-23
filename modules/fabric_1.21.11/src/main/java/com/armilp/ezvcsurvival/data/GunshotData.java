68d6f1c1f10f45c8aa85a00afb1a87370011bb08
SkyNetCloud <24949001+SkyNetCloud@users.noreply.github.com>
2026-04-22 22:08:48 -0400

    Fix later

--- modules/fabric_1.21.11/src/main/java/com/armilp/ezvcsurvival/data/GunshotData.java
+++ modules/fabric_1.21.11/src/main/java/com/armilp/ezvcsurvival/data/GunshotData.java
@@ -1,13 +1,13 @@
 package com.armilp.ezvcsurvival.data;
 
 import net.minecraft.util.math.Vec3d;
-
+//#todo fix later
 public class GunshotData {
-    public final Vec3d position;
-    public final long timestamp;
-
-    public GunshotData(Vec3d position, long timestamp, GunTabType gunType) {
-        this.position = position;
-        this.timestamp = timestamp;
-    }
+//    public final Vec3d position;
+//    public final long timestamp;
+//
+//    public GunshotData(Vec3d position, long timestamp, GunTabType gunType) {
+//        this.position = position;
+//        this.timestamp = timestamp;
+//    }
 }
